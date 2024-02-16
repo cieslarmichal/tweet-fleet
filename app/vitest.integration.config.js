54f7e7ba123f295b5f704cf7ea6835851f754bab
Michal Cieslar <michalovskyyy@gmail.com>
2024-02-16 22:47:11 +0100

    add configuration

---
+++ app/vitest.integration.config.js
@@ -0,0 +1,16 @@
+import { mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+
+import config from '../../vitest.config.js';
+
+export default mergeConfig(config, defineConfig({
+  test: {
+    setupFiles: ['./tests/setup.ts'],
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      }
+    }
+  },
+}));
