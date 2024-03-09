53743b2c062f738746eb650cbeb5197555542691
Michal Cieslar <michalovskyyy@gmail.com>
2024-03-09 14:41:37 +0100

    create sqs queues in processor stack

--- infra/src/stacks/processorStack.ts
+++ infra/src/stacks/processorStack.ts
@@ -1,6 +1,7 @@
 import * as core from 'aws-cdk-lib';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 import { NodejsLambda } from './common/nodejsLambda.js';
 import { type Config } from '../config.js';
@@ -16,6 +17,16 @@ export class ProcessorStack extends core.Stack {
     super(scope, id, props);
 
     const { config, subscriptionsTable, usersTable } = props;
+
+    const usersQueue = new sqs.Queue(this, 'UsersQueue', {
+      queueName: 'users',
+      visibilityTimeout: core.Duration.seconds(30),
+    });
+
+    const tweetsQueue = new sqs.Queue(this, 'TweetsQueue', {
+      queueName: 'tweets',
+      visibilityTimeout: core.Duration.seconds(30),
+    });
 
     const lambdaEnvironment = {
       ['SENDGRID_API_KEY']: config.sendGridApiKey,
@@ -35,5 +46,7 @@ export class ProcessorStack extends core.Stack {
     eventRule.addTarget(new targets.LambdaFunction(collectUsersLambda));
 
     usersTable.grantReadData(collectUsersLambda);
+
+    usersQueue.grantSendMessages(collectUsersLambda);
   }
 }
