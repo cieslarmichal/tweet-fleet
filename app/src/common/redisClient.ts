03acb0dcdcd56b804e8df02b7651d13a795ea7fe
Michal Cieslar <michalovskyyy@gmail.com>
2024-04-21 19:30:53 +0200

    improve redis client

--- app/src/common/redisClient.ts
+++ app/src/common/redisClient.ts
@@ -1,5 +1,7 @@
 import { Redis } from 'ioredis';
 
+import { type LoggerService } from './loggerService.js';
+
 export interface RedisClientConfig {
   readonly host: string;
   readonly port: number;
@@ -8,10 +10,41 @@ export interface RedisClientConfig {
 export type RedisClient = Redis;
 
 export class RedisClientFactory {
-  public static create(config: RedisClientConfig): RedisClient {
-    return new Redis({
+  public constructor(private readonly logger: LoggerService) {}
+
+  public create(config: RedisClientConfig): RedisClient {
+    const client = new Redis({
       host: config.host,
       port: config.port,
     });
+
+    client.on('connect', () => {
+      this.logger.info({ message: 'Redis client connected' });
+    });
+
+    client.on('ready', () => {
+      this.logger.info({ message: 'Redis client ready' });
+    });
+
+    client.on('error', (error) => {
+      this.logger.error({
+        message: 'Redis error',
+        error,
+      });
+    });
+
+    client.on('close', () => {
+      this.logger.info({ message: 'Redis client connection closed' });
+    });
+
+    client.on('reconnecting', () => {
+      this.logger.info({ message: 'Redis client reconnecting' });
+    });
+
+    client.on('end', () => {
+      this.logger.info({ message: 'Redis client connection ended' });
+    });
+
+    return client;
   }
 }
