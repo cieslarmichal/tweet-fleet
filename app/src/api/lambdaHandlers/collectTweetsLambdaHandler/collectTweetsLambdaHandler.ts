958bc44e04bb8cd9b53ebe8f6f1fdc8d9a1bd95a
Michal Cieslar <michalovskyyy@gmail.com>
2024-05-01 13:18:39 +0200

    add logic to send emails lambda handler

--- app/src/api/lambdaHandlers/collectTweetsLambdaHandler/collectTweetsLambdaHandler.ts
+++ app/src/api/lambdaHandlers/collectTweetsLambdaHandler/collectTweetsLambdaHandler.ts
@@ -1,6 +1,6 @@
 import { type Handler } from 'aws-lambda';
 
-import { SendUsersMessagesAction } from '../../../application/actions/sendUsersMessagesAction/sendUsersMessagesAction.js';
+import { SendSubscriptionTweetsMessagesAction } from '../../../application/actions/sendSubscriptionTweetsMessagesAction/sendSubscriptionTweetsMessagesAction.js';
 import { DynamoDbClientFactory } from '../../../common/dynamoDbClient.js';
 import { LoggerServiceFactory } from '../../../common/loggerService.js';
 import { SqsClientFactory } from '../../../common/sqsClient.js';
@@ -19,7 +19,7 @@ const logger = LoggerServiceFactory.create({
   logLevel: config.logLevel,
 });
 
-const action = new SendUsersMessagesAction(userRepository, sqsClient, logger, config);
+const action = new SendSubscriptionTweetsMessagesAction(userRepository, sqsClient, logger, config);
 
 export const lambda: Handler = async (): Promise<void> => {
   await action.execute();
--- app/src/api/lambdaHandlers/sendEmailsLambdaHandler/sendEmailsLambdaHandler.ts
+++ app/src/api/lambdaHandlers/sendEmailsLambdaHandler/sendEmailsLambdaHandler.ts
@@ -1,3 +1,55 @@
-import { type Handler } from 'aws-lambda';
+import { Type } from '@sinclair/typebox';
+import { Value } from '@sinclair/typebox/value';
+import { type Handler, type SQSEvent } from 'aws-lambda';
 
-export const lambda: Handler = async (): Promise<void> => {};
+import { SendSubscriptionEmailAction } from '../../../application/actions/sendSubscriptionEmailAction/sendSubscriptionsEmailAction.js';
+import { EmailService } from '../../../application/services/emailService/emailService.js';
+import { LoggerServiceFactory } from '../../../common/loggerService.js';
+import { ConfigFactory } from '../../../config/config.js';
+
+const config = ConfigFactory.create();
+
+const logger = LoggerServiceFactory.create({
+  logLevel: config.logLevel,
+});
+
+const emailService = new EmailService({
+  apiKey: config.sendGrid.apiKey,
+  senderEmail: config.sendGrid.senderEmail,
+});
+
+const eventBodySchema = Type.Object({
+  email: Type.String(),
+  tweets: Type.Array(
+    Type.Object({
+      id: Type.String(),
+      text: Type.String(),
+      createdAt: Type.String(),
+      urls: Type.Array(Type.String()),
+      selfUrl: Type.String(),
+      author: Type.Object({
+        id: Type.String(),
+        name: Type.String(),
+        username: Type.String(),
+        profileImageUrl: Type.String(),
+      }),
+    }),
+  ),
+});
+
+const action = new SendSubscriptionEmailAction(emailService, logger);
+
+export const lambda: Handler = async (event: SQSEvent): Promise<void> => {
+  const eventActions = event.Records.map(async (record) => {
+    const eventBody = JSON.parse(record.body).detail;
+
+    const { email, tweets } = Value.Decode(eventBodySchema, eventBody);
+
+    await action.execute({
+      email,
+      tweets,
+    });
+  });
+
+  await Promise.all(eventActions);
+};
