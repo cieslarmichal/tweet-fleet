033d68f924889c0d2ea0bd5f62ac4e7a652ec845
Michal Cieslar <michalovskyyy@gmail.com>
2024-05-01 13:09:27 +0200

    add logic to register user lambda handler

--- app/src/api/lambdaHandlers/registerUserLambdaHandler/registerUserLambdaHandler.ts
+++ app/src/api/lambdaHandlers/registerUserLambdaHandler/registerUserLambdaHandler.ts
@@ -1,32 +1,45 @@
+import { Type } from '@sinclair/typebox';
+import { Value } from '@sinclair/typebox/value';
 import { type APIGatewayEvent, type Handler, type ProxyResult } from 'aws-lambda';
 
-// const databaseName = process.env[EnvKey.databaseName] as string;
-// const host = process.env[EnvKey.databaseHost] as string;
-// const user = process.env[EnvKey.databaseUser] as string;
-// const databasePassword = process.env[EnvKey.databasePassword] as string;
-// const hashSaltRounds = parseInt(process.env[EnvKey.hashSaltRounds] as string);
+import { RegisterUserAction } from '../../../application/actions/registerUserAction/registerUserAction.js';
+import { HashService } from '../../../application/services/hashService/hashService.js';
+import { DynamoDbClientFactory } from '../../../common/dynamoDbClient.js';
+import { LoggerServiceFactory } from '../../../common/loggerService.js';
+import { ConfigFactory } from '../../../config/config.js';
+import { UserRepository } from '../../../domain/repositories/userRepository/userRepository.js';
 
-// const databaseQueryBuilder = new QueryBuilderFactoryImpl().create({
-//   databaseName,
-//   host,
-//   password: databasePassword,
-//   user,
-// });
+const dynamoDbClient = DynamoDbClientFactory.create();
 
-// const userMapper = new UserMapperImpl();
-// const userRepository = new UserRepositoryImpl(databaseQueryBuilder, userMapper);
-// const hashService = new HashServiceImpl(hashSaltRounds);
-// const registerUserCommand = new RegisterUserCommandImpl(hashService, userRepository);
+const userRepository = new UserRepository(dynamoDbClient);
+
+const config = ConfigFactory.create();
+
+const logger = LoggerServiceFactory.create({
+  logLevel: config.logLevel,
+});
+
+const hashService = new HashService({ hashSaltRounds: config.hashSaltRounds });
+
+const action = new RegisterUserAction(userRepository, hashService, logger);
+
+const bodySchema = Type.Object({
+  email: Type.String(),
+  password: Type.String(),
+});
 
 export const lambda: Handler = async (event: APIGatewayEvent): Promise<ProxyResult> => {
-  // const { email, emailPassword, password } = JSON.parse(event.body as string);
+  const body = JSON.parse(event.body as string);
 
-  // await registerUserCommand.registerUser({ email, emailPassword, password });
+  const { email, password } = Value.Decode(bodySchema, body);
 
-  console.log({ event });
+  await action.execute({
+    email,
+    password,
+  });
 
   return {
-    statusCode: 201,
+    statusCode: 200,
     body: '',
   };
 };
